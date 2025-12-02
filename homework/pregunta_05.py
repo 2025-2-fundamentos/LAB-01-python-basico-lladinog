85240ba64c82fd6ccd83407c7fc7e06702e81a2e
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:54:41 -0500

    Implement data processing from CSV in pregunta_05.py
    Read data from a CSV file, process it, and return sorted results.

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -15,3 +15,25 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
+
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+    
+    columns = list(map(lambda line: line.split('\t'), lines))
+    columns1_2 = sorted(list(map(lambda column: (column[0], int(column[1])), columns)))
+
+    result = []
+    for key, value in columns1_2:
+        if result and result[-1][0] == key:
+            if value > result[-1][1]:
+                result[-1] = (key, value, result[-1][2])
+            elif value < result[-1][2]:
+                result[-1] = (key, result[-1][1], value)
+        else:
+            result.append((key, value, value))
+    
+    return result
+
+pregunta_05()
