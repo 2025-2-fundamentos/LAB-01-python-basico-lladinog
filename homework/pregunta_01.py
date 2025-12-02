b4deb09246291918fa389a772ec9a009c5efb365
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:53:21 -0500

    Add function to sum second column from CSV
    Implement function to sum values from the second column of a CSV file.

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,7 +5,6 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
@@ -14,3 +13,14 @@ def pregunta_01():
     214
 
     """
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+        
+    columns = list(map(lambda line: line.split('\t'), lines))
+    columna2 = list(map(lambda column: column[1], columns))
+    columna2_int = list(map(lambda x: int(x), columna2))
+    return sum(columna2_int)
+        
+pregunta_01()
