45610628a3dcea458d835aff20d8e02f8e96f308
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:57:30 -0500

    Implement dictionary generation from CSV data
    Added functionality to generate a dictionary summing values from column 5 based on column 1.

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -6,6 +6,9 @@ utilizar pandas, numpy o scipy.
 """
 
 
+from pprint import pprint
+
+
 def pregunta_12():
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor
@@ -15,3 +18,23 @@ def pregunta_12():
     {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
 
     """
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+    
+    columns = list(map(lambda line: line.split('\t'), lines))
+    columns1_5 = list(map(lambda column: (column[0], list(map(lambda x: int(x.split(':')[1]) ,column[4].replace("\n", "").split(',')))), columns))
+    sorted_columns = sorted(columns1_5)
+    
+    dict = {}
+    for key, value in sorted_columns:
+        for x in value:
+            if key not in dict:
+                dict[key] = x
+            else:
+                dict[key] += x
+    
+    return dict
+
+pregunta_12()
