b29ec5f9f1af053ec5dbc3c9266db2f2ac44d78b
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:53:41 -0500

    Add functionality to read and count column data

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -15,3 +15,23 @@ def pregunta_02():
     [('A', 8), ('B', 7), ('C', 5), ('D', 6), ('E', 14)]
 
     """
+
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+    
+    columns = list(map(lambda line: line.split('\t'), lines))
+    column1 = list(map(lambda x: x[0], columns))
+    column1_sorted = sorted(column1)
+    
+    result = []
+    for x in column1_sorted:
+        if result and result[-1][0] == x:
+            result[-1] = (x, result[-1][1] + 1) 
+        else:
+            result.append((x, 1))
+    
+    return result
+
+pregunta_02()
