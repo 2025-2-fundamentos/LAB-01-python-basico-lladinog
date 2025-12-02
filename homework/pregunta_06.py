ba9189c80cc879ee5323ae4371e87fe66d4aa742
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:55:10 -0500

    Refactor pregunta_06 to process CSV input data
    Refactor pregunta_06 function to read and process data from a CSV file, sort dictionaries, and return results based on specified conditions.

--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -26,3 +26,34 @@ def pregunta_06():
      ('jjj', 5, 17)]
 
     """
+
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+    
+    columns = list(map(lambda line: line.split('\t'), lines))
+    column5 = list(map(lambda column: column[4].replace("\n", ""), columns))
+    dicts_in_columns = list(map(lambda column5: column5.split(','), column5))
+    dicts = []
+    for column in dicts_in_columns:
+        for dict in column:
+            dicts.append(dict)
+
+    dicts_sorted = sorted(dicts)
+    pairs = list(map(lambda dict: dict.split(':'), dicts_sorted))
+    pairs = list(map(lambda dict: (dict[0], int(dict[1])), pairs))
+
+    result = []
+    for key, value in pairs:
+        if result and result[-1][0] == key:
+            if value < result[-1][1]:
+                result[-1] = (key, value, result[-1][2])
+            elif value > result[-1][2]:
+                result[-1] = (key, result[-1][1], value)
+        else:
+            result.append((key, value, value))
+
+    return result
+
+pregunta_06()
