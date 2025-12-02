596e8812b03356c592f4eb10c5b79f68e2fdb029
lladinog <159490210+lladinog@users.noreply.github.com>
2025-12-02 11:54:04 -0500

    Read and process data from CSV file

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,23 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+
+    lines = []
+    with open ("files/input/data.csv", "r", encoding="utf-8") as f:
+        for line in f:
+            lines.append(line)
+    
+    columns = list(map(lambda line: line.split('\t'), lines))
+
+    pairs = list(map(lambda column: (column[0], int(column[1])), columns))
+    pairs_sorted = sorted(pairs)
+
+    result = []
+    for key, value in pairs_sorted:
+        if result and result[-1][0] == key:
+            result[-1] = (key, result[-1][1] + value)
+        else:
+            result.append((key, value))
+    return result
+
+pregunta_03()
